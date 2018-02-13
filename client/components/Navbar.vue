79382803b87e7583abd3de17ec5acbe940938279
Ned Palacios <npdoesmc@gmail.com>
2018-02-13 17:29:34 +0800

    redesigning navbar for 0.6.2 bulma

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -1,77 +1,71 @@
 <template>
-	<nav class="nav has-shadow" style="position:fixed;top:0;left:0;right:0;">
-		<div class="nav-left">
-			<router-link :to="{ name: 'home' }" class="nav-item">
-				MANGAVUE
+	<nav class="navbar is-fixed-top" :class="{ 'is-transparent transparent-bg': isHome }">
+		<div class="navbar-brand">
+			<router-link v-if="this.$route.name !== 'home'" :to="{ name: 'home' }" class="navbar-item">
+				<img src="../../static/mascot.png"/>
+        <span style="margin-left:1rem;">MANGAVUE</span>
 			</router-link>
+
+      <button class="button navbar-burger" @click="showNav = !showNav" :class="{ 'is-active' : showNav }">
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
 		</div>
-
-		<label class="nav-toggle" for="search-toggle-state">
-				<i class="fa fa-search" style="margin-top:13px"></i>
-		</label>
-
-		<input type="checkbox" id="search-toggle-state" />
-
-		<label class="nav-toggle" for="nav-toggle-state">
-			<span></span>
-			<span></span>
-			<span></span>
-		</label>
-
-		<input type="checkbox" id="nav-toggle-state" />
-
-	    <div class="nav-center search-box" v-if="this.$route.name !== 'home'">
-	    	<div class="nav-item">
-		    	<template v-if="this.$route.name == 'search'">
-		      		<search-box v-bind:value="sQuery"></search-box>
-		      	</template>
-		      	<template v-else>
-		      		<search-box v-bind:isLoading="loading" v-bind:value="searchQuery"></search-box>
-		      	</template>
-	      	</div>
-	    </div>
-	    <div class="nav-right nav-menu">
-	      <a href="https://vuejs.org" class="nav-item is-tab">
-	        Vue.js
-	      </a>
-	      <div class="nav-item">
-		      <div class="field">
-		      	<p class="control">
-		      		<a class="button" href="https://twitter.com/npned">
-								<span class="icon is-small">
-						      <i class="fa fa-twitter"></i>
-						    </span>
-								<span>@npned</span>
-							</a>
-		      	</p>
-		      </div>
-	      </div>
-	    </div>
+    <div class="navbar-menu" :class="{ 'is-active' : showNav }">
+      <div class="navbar-start search-box" v-if="this.$route.name !== 'home'">
+        <div class="navbar-item">
+          <template v-if="this.$route.name == 'search'">
+            <search-box v-bind:value="sQuery"></search-box>
+          </template>
+          <template v-else>
+            <search-box v-bind:isLoading="loading" v-bind:value="searchQuery"></search-box>
+          </template>
+        </div>
+      </div>
+      <div class="navbar-end">
+        <a href="https://vuejs.org" class="navbar-item is-tab">
+          Vue.js
+        </a>
+        <div class="navbar-item">
+          <div class="field">
+            <p class="control">
+              <a class="button" href="https://twitter.com/npned">
+                <span class="icon is-small">
+                  <i class="fa fa-twitter"></i>
+                </span>
+                <span>@npned</span>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
 	</nav>
 </template>
 
 <style>
-nav > .wrapper.container {
-	margin-bottom: 20px !important;
+.transparent-bg {
+  background: transparent;
+}
+
+.button.navbar-burger {
+  border-radius: inherit;
+  border: inherit;
+}
+
+.button.navbar-burger:focus,
+.button.navbar-burger:hover {
+  box-shadow: inherit;
+  border: none;
 }
 
 .search-box {
 	display: flex;
 }
 
-
-#nav-toggle-state,
-#search-toggle-state {
-  display: none;
-}
-
-#nav-toggle-state:checked ~ .nav-menu,
-#search-toggle-state:checked ~ .search-box {
-  display: block;
-}
-
 @media screen and (max-width: 768px) {
-	.nav-center.search-box {
+	.navbar-start.search-box {
 		background-color: white;
 		box-shadow: 0 4px 7px rgba(10, 10, 10, 0.1);
 		display: none;
@@ -91,13 +85,33 @@ import { mapState } from 'vuex'
 export default {
 	components: {
 		SearchBox
-	},
+  },
+  watch: {
+    '$route': 'insertFixedTag'
+  },
+  data() {
+    return {
+      showNav: false
+    }
+  },
 	computed: {
 		...mapState({
 			searchQuery: state => state.search.query,
 			sQuery: state => state.route.query.q,
 			loading: state => state.globals.isLoading
-		})
-	}
+		}),
+    isHome() {
+      return this.$route.name == 'home' ? true : false
+    }
+	},
+  methods: {
+    insertFixedTag() {
+      if (!this.isHome) {
+        const root = document.getElementsByTagName('html')[0]
+        root.setAttribute('class', 'has-navbar-fixed-top')
+        document.getElementsByTagName('body')[0].setAttribute('class', 'has-navbar-fixed-top')
+      }
+    }
+  }
 }
 </script>
