f85081a1d6220c5a335ef2593082d643ab1b31a2
Ned Palacios <npdoesmc@gmail.com>
2018-02-13 17:32:20 +0800

    redesigned homepage

--- client/views/Home.vue
+++ client/views/Home.vue
@@ -1,25 +1,33 @@
 <template>
-<div style="padding:1rem;">
-	<div class="columns">
-		<div class="column is-8 is-offset-2  has-text-centered">
-			<h1 class="is-hidden-tablet title is-2">Manga Reader powered by Vue.</h1>
-			<h1 class="is-hidden-mobile title is-1">Manga Reader powered by Vue.</h1>
-			<search-box-big v-bind:value="searchQuery"></search-box-big>
-		</div>
-	</div>
-	<div class="columns">
-		<div class="column is-10 is-offset-1 has-text-centered">
-			<ul style="list-style-type:none;">
-				<li style="display: inline; margin-right: 2px;" v-for="genre in genres">
-					<router-link :to="{name: 'genre', params: {genreName: genre.genreId}}">
-						<span style="margin: 2px;" class="tag is-medium is-primary">{{ genre.genreId }}</span>
-					</router-link>
-				</li>
-			</ul>
-		</div>
-	</div>
+<div id="fullpage">
+    <div class="section" style="padding-bottom:0;">
+      <div class="columns">
+        <div class="column is-8 is-offset-2  has-text-centered">
+          <h1><img src="../../static/mascot.png" style="max-width: 30%;" /></h1>
+          <h1 class="title is-1">Mangavue</h1>
+          <h1 class="subtitle">Manga Reader powered by Vue.</h1>
+          <search-box-big v-bind:value="searchQuery"></search-box-big>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-11 is-offset-1 has-text-centered">
+          <ul style="list-style-type:none;">
+            <li style="display: inline; margin-right: 2px;" :key="genre.genreId" v-for="genre in genres">
+              <router-link :to="{name: 'genre', params: {genreName: genre.genreId}}">
+                <span style="margin: 2px;" class="tag is-medium is-primary">{{ genre.genreId }}</span>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
 </div>
 </template>
+<style scoped>
+body {
+  margin: 0 !important;
+}
+</style>
 
 <script>
 import SearchBoxBig from 'components/SearchBoxBig'
@@ -30,7 +38,7 @@ export default {
     SearchBoxBig
   },
   mounted() {
-			this.$store.dispatch('LOAD_GENRES');
+    this.$store.dispatch('LOAD_GENRES')
   },
   computed: {
 		...mapState({
